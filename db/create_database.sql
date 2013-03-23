7e69d37533ff3bba0dfe4334df2a53b38fff47b1
alex <alex@alex-HP-Pavilion-dv5-Notebook-PC.(none)>
2013-03-23 21:30:14 +0400

    modified create db script

--- db/create_database.sql
+++ db/create_database.sql
@@ -7,7 +7,7 @@ CREATE TABLE `makers` (
 	country VARCHAR(255)
 );
 
-CREATE TABLE `medicine` (
+CREATE TABLE `medicines` (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(255) NOT NULL,
 	is_recipe BOOLEAN NOT NULL DEFAULT 0,
@@ -21,3 +21,18 @@ CREATE TABLE `pharmacies` (
 	start_at TIMESTAMP NOT NULL,
 	end_at TIMESTAMP NOT NULL
 );
+
+CREATE TABLE `medicines_makers` (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	medicine_id INT NOT NULL,
+	maker_id INT NOT NULL
+);
+
+CREATE TABLE `medicines_makers_pharmacies` (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	medicine_id INT NOT NULL,
+	maker_id INT NOT NULL,
+	pharmacy_id INT NOT NULL,
+	cost INT NOT NULL,
+);
+
