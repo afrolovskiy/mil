0da069341b3bf2446106caf085d3c83a68b671d1
root <root@mysql.(none)>
2013-05-08 07:09:29 +0000

    temp

--- db/medicines/urls.py
+++ db/medicines/urls.py
@@ -1,9 +1,12 @@
-from django.conf.urls import patterns
+from django.conf.urls import *
 from django.views.generic import TemplateView
-
+from django.contrib import admin
 from gui.views import (
     MedicinesView, PharmaciesView, MakersView, MakerMedicinesView,
 	PharmacyMakerMedicinesView)
+
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^medicnes$', MedicinesView.as_view()),
@@ -11,4 +14,5 @@ urlpatterns = patterns('',
     (r'^pharmacies/costs$', PharmacyMakerMedicinesView.as_view()),
     (r'^makers$', MakersView.as_view()),
     (r'^makers/(?P<maker_id>\d{1,5})/pharmacies$', MakerMedicinesView.as_view()),
+    (r'^admin/', include(admin.site.urls)),    
 )
